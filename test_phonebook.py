29f01808e43b4ace1efab371e64aba7ad1face0b
mwalughabura <mwalughabura@gmail.com>
2018-03-01 16:59:14 +0300

    Finalized the task

--- test_phonebook.py
+++ test_phonebook.py
@@ -4,11 +4,12 @@ import phonebook
 class SavePhoneNumber(unittest.TestCase):
 
 	def test_get_name(self):
-		result = phonebook.name("Mwalugha")
+		result = phonebook.get_name("Mwalugha")
 		result.assertTrue(len(name) > 1)
 
 	def test_get_phone_no(self):
-		pass
+		result = phonebook.get_phone_no(58768687)
+		result.assertTrue(len(str(58768687)) > 10)
 
 if __name__ == '__main__':
 	unittest.main()
