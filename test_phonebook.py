cb991885466b6d3457a16921c3f72d49dd8d2e20
mwalughabura <mwalughabura@gmail.com>
2018-03-01 16:51:24 +0300

    Add the features in the test file     - The file tests for the truth of the input value

--- test_phonebook.py
+++ test_phonebook.py
@@ -5,6 +5,7 @@ class SavePhoneNumber(unittest.TestCase):
 
 	def test_get_name(self):
 		result = phonebook.name("Mwalugha")
+		result.assertTrue(len(name) > 1)
 
 	def test_get_phone_no(self):
 		pass
